ccb0a6dd66cf519339e380d89b48a33c7d61d33d
Louis <louis@planhub.ca>
2023-04-21 13:45:56 -0400

    test woindowPost

--- esim-plans-en.js
+++ esim-plans-en.js
@@ -1,65 +1,77 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("message", function (event) {
+    // if (event.origin !== 'https://example.com') { // replace with the domain of file1
+    //   return; // reject messages from other domains
+    // }
+    console.log(event.data); // log the message content
+    console.log(coucou)
+  });
+  
+  
+  document.addEventListener("DOMContentLoaded", function () {
     const simbudLink = document.querySelector("a[href*='https://www.simbud.com']");
     let elWidgets = document.querySelectorAll(".simbud-esim-plans-widget");
-    
+  
+  
     elWidgets.forEach(elWidget => {
-        console.log(simbudLink)
-        if (linkIsUpAndFollows(simbudLink)) {
-            let newIframe = document.createElement('iframe');
-            let dataWidget = elWidget.getAttribute("data-widget");
-            let url = "https://cute-syrniki-030b45.netlify.app/" //Make sure to update to real url
-
-            // Fetch to verify the number of offers and adapt the size of the iframe
-            fetch(url)
-                .then(response => {
-                    if (response.ok) {
-                        return response.text();
-                    } else {
-                        throw new Error('Error: Network response was not ok');
-                    }
-                })
-                .then(data => {
-                    let widgetHeight = calculateWidgetHeight(data);
-                
-                    // Set the iframe srcdoc attribute with the fetched HTML content
-                    newIframe.src = url;
-                
-                    newIframe.className = "widgetIframeElement";
-                    newIframe.style = `width: 100%; border: 0; margin: 0 auto; display: block; height: ${widgetHeight}px`;
-                    newIframe.setAttribute("data-widget", dataWidget);
-                
-                    elWidget.prepend(newIframe);
-                    elWidget.removeChild(elWidget.lastElementChild);
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-        } else {
-            let errorMessage = document.createElement('H3');
-            errorMessage.innerHTML = 'Error: Element missing';
-            elWidget.prepend(errorMessage);
-        }
+      if (linkIsUpAndFollows(simbudLink)) {
+        let newIframe = document.createElement('iframe');
+        let dataWidget = elWidget.getAttribute("data-widget");
+        let url = "https://cute-syrniki-030b45.netlify.app/" //Make sure to update to real url
+  
+        // Fetch to verify the number of offers and adapt the size of the iframe
+        fetch(url)
+          .then(response => {
+            if (response.ok) {
+              return response.text();
+            } else {
+              throw new Error('Error: Network response was not ok');
+            }
+          })
+          .then(data => {
+            console.log(data)
+            let widgetHeight = calculateWidgetHeight(data);
+  
+            // Set the iframe srcdoc attribute with the fetched HTML content
+            newIframe.src = url;
+            newIframe.id = "eda"
+            newIframe.className = "widgetIframeElement";
+            newIframe.style = `width: 100%; border: 0; margin: 0 auto; display: block; height: ${widgetHeight}px`;
+            newIframe.setAttribute("data-widget", dataWidget);
+  
+            elWidget.prepend(newIframe);
+            elWidget.removeChild(elWidget.lastElementChild);
+  
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+      } else {
+        let errorMessage = document.createElement('H3');
+        errorMessage.innerHTML = 'Error: Element missing';
+        elWidget.prepend(errorMessage);
+      }
     });
-
-});
-
-const linkIsUpAndFollows = (link) => {
-    console.log(link)
+  
+  });
+  
+  const linkIsUpAndFollows = (link) => {
     return link && !link.getAttribute('rel')?.match(/nofollow/i);
-}
-
-const calculateWidgetHeight = (data) => {
+  }
+  
+  const calculateWidgetHeight = (data) => {
     let widgetHeight = 310;
-
+  
     // Transformation de la reponse
     const parser = new DOMParser();
     const doc = parser.parseFromString(data, "text/html");
-
+  
     // Comptage du nombre d'offres, en multiple de 3
-    const count = doc.getElementsByClassName("proposal_plans__box").length;
+    const count = doc.getElementsByClassName("Card_card__9h8Dh").length;
     if (count > 3) {
-        widgetHeight += Math.floor((count - 1) / 3) * 210;
+      widgetHeight += Math.floor((count - 1) / 3) * 210;
     }
+  
+    console.log(count)
 
     if (count === 0) {
         widgetHeight = 180;
