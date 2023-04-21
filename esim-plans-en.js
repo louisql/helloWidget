34bd8c7d99bff560fe321ccb1d3d41d631b71b1d
Louis <louis@planhub.ca>
2023-04-21 15:31:17 -0400

    retrait fetch

--- esim-plans-en.js
+++ esim-plans-en.js
@@ -3,72 +3,58 @@ let widgetHeight;
 let newIframe = document.createElement('iframe');
 
 document.addEventListener("DOMContentLoaded", function () {
-  window.addEventListener("message", function (event) {
-    nbreOfOffers = event.data;
-    if (Number.isInteger(nbreOfOffers)) {
-      widgetHeight = calculateWidgetHeight(nbreOfOffers);
-      newIframe.style = `width: 100%; border: 0; margin: 0 auto; display: block; height: ${widgetHeight}px`;
-      elWidget.prepend(newIframe);
-      elWidget.removeChild(elWidget.lastElementChild);
-    }
-  });
+    window.addEventListener("message", function (event) {
+        nbreOfOffers = event.data;
 
-  const simbudLink = document.querySelector("a[href*='https://www.simbud.com']");
-  let elWidgets = document.querySelectorAll(".simbud-esim-plans-widget");
+        //Was required to reset the parameters as two messages are coming from the react app, initial message empty, second one with relevant parameters
+        if (Number.isInteger(nbreOfOffers)) {
+            widgetHeight = calculateWidgetHeight(nbreOfOffers);
+            newIframe.style = `width: 100%; border: 0; margin: 0 auto; display: block; height: ${widgetHeight}px`;
+            elWidget.prepend(newIframe);
+            elWidget.removeChild(elWidget.lastElementChild);
+        }
+    });
 
-  elWidgets.forEach(elWidget => {
-    if (linkIsUpAndFollows(simbudLink)) {
-      let dataWidget = elWidget.getAttribute("data-widget");
-      let url = "https://cute-syrniki-030b45.netlify.app/" //Make sure to update to real url
+    const simbudLink = document.querySelector("a[href*='https://www.simbud.com']");
+    let elWidgets = document.querySelectorAll(".simbud-esim-plans-widget");
 
-      fetch(url)
-        .then(response => {
-          if (response.ok) {
-            return response.text();
-          } else {
-            throw new Error('Error: Network response was not ok');
-          }
-        })
-        .then(data => {
-          nbreOfOffers = parseInt(data); // parse the fetched data as integer
-          widgetHeight = calculateWidgetHeight(nbreOfOffers);
+    elWidgets.forEach(elWidget => {
+        if (linkIsUpAndFollows(simbudLink)) {
+            let dataWidget = elWidget.getAttribute("data-widget");
 
-          newIframe.src = url;
-          newIframe.id = "eda";
-          newIframe.className = "widgetIframeElement";
-          newIframe.style = `width: 100%; border: 0; margin: 0 auto; display: block; height: ${widgetHeight}px`;
-          newIframe.setAttribute("data-widget", dataWidget);
+            //Make sure to update to real url
+            let url = "https://cute-syrniki-030b45.netlify.app/"
+            newIframe.src = url;
+            newIframe.id = "eda";
+            newIframe.className = "widgetIframeElement";
+            newIframe.style = `width: 100%; border: 0; margin: 0 auto; display: block; height: 320px`;
+            newIframe.setAttribute("data-widget", dataWidget);
+            elWidget.prepend(newIframe);
+            elWidget.removeChild(elWidget.lastElementChild);
 
-          elWidget.prepend(newIframe);
-          elWidget.removeChild(elWidget.lastElementChild);
-
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    } else {
-      let errorMessage = document.createElement('H3');
-      errorMessage.innerHTML = 'Error: Element missing';
-      elWidget.prepend(errorMessage);
-    }
-  });
+        } else {
+            let errorMessage = document.createElement('H3');
+            errorMessage.innerHTML = 'Error: Element missing';
+            elWidget.prepend(errorMessage);
+        }
+    });
 
 });
 
 const linkIsUpAndFollows = (link) => {
-  return link && !link.getAttribute('rel')?.match(/nofollow/i);
+    return link && !link.getAttribute('rel')?.match(/nofollow/i);
 }
 
 const calculateWidgetHeight = (nbreOfOffers) => {
-  let widgetHeight = 320;
+    let widgetHeight = 320;
 
-  if (nbreOfOffers > 3) {
-    widgetHeight += Math.floor((nbreOfOffers - 1) / 3) * 220;
-  }
+    if (nbreOfOffers > 3) {
+        widgetHeight += Math.floor((nbreOfOffers - 1) / 3) * 220;
+    }
 
-  if (nbreOfOffers === 0) {
-    widgetHeight = 180;
-  }
+    if (nbreOfOffers === 0) {
+        widgetHeight = 180;
+    }
 
-  return widgetHeight;
+    return widgetHeight;
 }
