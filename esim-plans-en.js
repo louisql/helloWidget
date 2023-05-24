48b6ede5b84e8ef6e4dccd5f0ac0d4ad35a2838b
louisql <louis.queruau@gmail.com>
2023-05-24 13:56:28 -0400

    simplyfying code, receiving height from react app

--- esim-plans-en.js
+++ esim-plans-en.js
@@ -1,37 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let nbreOfOffers;
+    let reactAppHeight;
     let widgetHeight;
     let newIframe = document.createElement('iframe');
 
-    //Receiving message from the react website with the nber of offers
-    window.addEventListener("message", (event) => {
-        nbreOfOffers = event.data;
-        // console.log(nbreOfOffers)
-        
-        //Was required to set the parameters here and in loops as two messages are coming from the react app, initial message empty, second one with relevant parameters
-        if (Number.isInteger(nbreOfOffers)) {
-            widgetHeight = calculateWidgetHeight(nbreOfOffers);
-            newIframe.style = `; border: 0; margin: 0 auto;top:0px; width:100%; height:${widgetHeight}px;    `;
-            elWidget.prepend(newIframe);
-            elWidget.removeChild(elWidget.lastElementChild);
-        }
-    });
-
-    const simbudLink = document.querySelector("a[href*='https://www.simbud.com']");
+    let simbudLink = document.querySelector("a[href*='https://www.simbud.com']");
     let elWidgets = document.querySelectorAll(".simbud-esim-plans-widget");
 
+    //Make sure to update to real url !
+    let url = "http://localhost:3000/"
+    // let url = "https://cute-syrniki-030b45.netlify.app/"
+    newIframe.src = url;
+    newIframe.id = "eda";
+    newIframe.className = "widgetIframeElement";
+    //Setting basic style for initial loading
+    newIframe.style = `width: 100%; border: 0; margin: 0 auto; display: block; height: ${widgetHeight}px`;
+    
     elWidgets.forEach(elWidget => {
         if (linkIsUpAndFollows(simbudLink)) {
             let dataWidget = elWidget.getAttribute("data-widget");
+            console.log(dataWidget)
 
-            //Make sure to update to real url !
-            // let url = "http://localhost:3000/"
-            let url = "https://cute-syrniki-030b45.netlify.app/"
-            newIframe.src = url;
-            newIframe.id = "eda";
-            newIframe.className = "widgetIframeElement";
-            //Setting basic style for initial loading
-            newIframe.style = `width: 100%; border: 0; margin: 0 auto; display: block; height: ${widgetHeight}px`;
             newIframe.setAttribute("data-widget", dataWidget);
             elWidget.prepend(newIframe);
             elWidget.removeChild(elWidget.lastElementChild);
@@ -43,6 +31,20 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    //Receiving message from the react website with the nber of offers
+    window.addEventListener("message", (event) => {
+        reactAppHeight = event.data;
+        // console.log(reactAppHeight)
+        
+
+        //Was required to set the parameters here and in loops as two messages are coming from the react app, initial message empty, second one with relevant parameters
+        if (Number.isInteger(reactAppHeight)) {
+            widgetHeight = calculateWidgetHeight(reactAppHeight);
+            newIframe.style = `; border: 0; margin: 0 auto;top:0px; width:100%; height:${widgetHeight}px;    `;
+            elWidget.prepend(newIframe);
+            elWidget.removeChild(elWidget.lastElementChild);
+        }
+    });
 });
 
 const linkIsUpAndFollows = (link) => {
@@ -50,59 +52,6 @@ const linkIsUpAndFollows = (link) => {
 }
 
 const calculateWidgetHeight = (data) => {
-    let width = window.innerWidth;
-    let widgetHeight = 420
-
-    // Counting number of offers, in multiple of 3
-    const count = data;
-
-    // Adjusting height depending on number of offers
-
-    //3 offers or below
-    if (count === 3) {
-        if (width > 932) {
-            widgetHeight = 420
-        } else if (width > 720 && width < 932) {
-            widgetHeight = 660
-        } else if (width<630 && width > 610) {
-            widgetHeight = 740
-        } else if (width>610) {
-            widgetHeight = 725
-        } else if (width > 450){
-            widgetHeight = 960
-        } else {
-            widgetHeight = 1010
-        }
-    } else if (count === 2) {
-        if (width < 700 && width > 610) {
-            widgetHeight = 470;
-        } else if (width > 450) {
-            widgetHeight = 710
-        } else {
-            widgetHeight = 770
-        }
-    } else if (count === 1) {
-        if (width > 700) widgetHeight = 420
-        else if (width> 430) widgetHeight = 470
-        else widgetHeight = 540
-    }
-
-    //Over 3 offers
-    if (count > 3 && width > 920) {
-        widgetHeight += Math.floor((count - 1) / 3) * 260;
-    } else if (count > 3 && width > 650) {
-
-        widgetHeight += Math.floor((count - 1) / 3) * 540;
-    } else if (count > 3) {
-        widgetHeight += Math.floor((count - 1) / 3) * 1050;
-    }
-
-    //No offer 
-    if (count === 0 ) {
-        if ( width > 700 ) widgetHeight = 300;
-        else if (width > 440) widgetHeight = 400
-        else widgetHeight = 250
-    }
-
+    widgetHeight= data + 50
     return widgetHeight;
 }
