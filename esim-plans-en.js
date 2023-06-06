d938154c6c26e71d6a2a6820b42bbaa649ca4f2e
louisql <louis.queruau@gmail.com>
2023-06-06 17:23:30 -0400

    adding currency to url

--- esim-plans-en.js
+++ esim-plans-en.js
@@ -5,27 +5,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let simbudLink = document.querySelector("a[href*='https://www.simbud.com']");
     let elWidgets = document.querySelectorAll(".simbud-esim-plans-widget");
-
-    //Sending the country and the referal to the react app
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const urlCountry = urlParams.get('country')
-    const urlReferal = urlParams.get('referal')
-
-    //Setting Canada as default country if no country is passed
-    const country = (urlCountry === null ? 'Canada' : urlCountry.charAt(0).toUpperCase() + urlCountry.slice(1));
-    const referal = (urlReferal === null ? '' : urlReferal.toLocaleLowerCase());
-
-    //Make sure to update to real url !
-    // let url = "http://localhost:3000/"+country+"&referal="+referal
-    let url = "https://simbud.com/widget/?country="+country+"&referal="+referal
-    newIframe.src = url;
-    newIframe.id = "eda";
-    newIframe.className = "widgetIframeElement";
-    //Setting basic style for initial loading
-    newIframe.style = `width: 100%; border: 0; margin: 0 auto; display: block; height: ${widgetHeight}px`;
+    
 
     elWidgets.forEach(elWidget => {
+        const country = elWidget.dataset.country || 'Canada';
+        const referal = elWidget.dataset.referal || '';
+        const currency = elWidget.dataset.currency || 'CAD';
+
+        let url = "https://simbud.com/widget/?country=" + country + "&referal=" + referal + "&currency=" + currency;
+
+        newIframe.src = url;
+        newIframe.id = "eda";
+        newIframe.className = "widgetIframeElement";
+        //Setting basic style for initial loading
+        newIframe.style = `width: 100%; border: 0; margin: 0 auto; display: block; height: ${widgetHeight}px`;
+
         if (linkIsUpAndFollows(simbudLink)) {
             let dataWidget = elWidget.getAttribute("data-widget");
 
@@ -40,16 +34,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    //Receiving message from the react website with the nber of offers
+    //Receiving message from the react website with the number of offers
     window.addEventListener("message", (event) => {
         reactAppHeight = event.data;
-        // console.log(reactAppHeight)
-
-
-        //Was required to set the parameters here and in loops as two messages are coming from the react app, initial message empty, second one with relevant parameters
         if (Number.isInteger(reactAppHeight)) {
             widgetHeight = calculateWidgetHeight(reactAppHeight);
-            newIframe.style = `; border: 0; margin: 0 auto;top:0px; width:100%; height:${widgetHeight}px;    `;
+            newIframe.style = `border: 0; margin: 0 auto;top:0px; width:100%; height:${widgetHeight}px;`;
             elWidget.prepend(newIframe);
             elWidget.removeChild(elWidget.lastElementChild);
         }
@@ -61,6 +51,6 @@ const linkIsUpAndFollows = (link) => {
 }
 
 const calculateWidgetHeight = (data) => {
-    widgetHeight = data + 50
+    widgetHeight = data + 50;
     return widgetHeight;
 }
